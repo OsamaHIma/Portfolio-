188bc949416183e079fbcd3ab1e3f449f4f82854
Osama magdy <108794970+OsamaHIma@users.noreply.github.com>
2022-12-02 14:14:13 +0200

    Creating the files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import * as THREE from "three";
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <canvas className="Canvas">
+
+      </canvas>
+    </div>
+  );
+}
+
+export default App;
